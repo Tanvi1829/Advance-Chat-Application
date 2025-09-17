8988bd7f78bdf97f03a460c0268a63391511176c
Tanvi1829 <mapuskartanvi6@gmail.com>
2025-09-17 13:13:40 +0530

    usechatstore changes

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { useAuthStore } from "./useAuthStore";
-import axiosInstance from "../lib/axios";
 
 export const useChatStore = create((set, get) => ({
   allContacts: [],
@@ -21,18 +21,17 @@ export const useChatStore = create((set, get) => ({
   setActiveTab: (tab) => set({ activeTab: tab }),
   setSelectedUser: (selectedUser) => set({ selectedUser }),
 
-getAllContacts: async () => {
-  set({ isUsersLoading: true });
-  try {
-    const res = await axiosInstance.get("/messages/contacts");
-    set({ allContacts: res.data });
-  } catch (error) {
-    toast.error(error.response?.data?.message || "Cannot fetch contacts");
-  } finally {
-    set({ isUsersLoading: false });
-  }
-},
-
+  getAllContacts: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/contacts");
+      set({ allContacts: res.data });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Cannot fetch contacts");
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
   getMyChatPartners: async () => {
     set({ isUsersLoading: true });
     try {
