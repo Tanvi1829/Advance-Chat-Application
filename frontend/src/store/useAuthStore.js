8fbed82580400540ae157e261655853ab536a0ef
Tanvi1829 <mapuskartanvi6@gmail.com>
2025-09-17 16:44:48 +0530

    chat auth store

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-import axiosInstance  from "../lib/axios";
+import {axiosInstance}  from "../lib/axios";
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : import.meta.env.VITE_API_URL;;
 
