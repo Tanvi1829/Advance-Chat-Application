4f815c8ef514727c571d54a69cf3f6de625dfc40
Tanvi1829 <mapuskartanvi6@gmail.com>
2025-09-17 13:29:36 +0530

    same changes again

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
-import { axiosInstance } from "../lib/axiosInstance";
+import { axiosInstance } from "../lib/axios";
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : import.meta.env.VITE_API_URL;;
 
