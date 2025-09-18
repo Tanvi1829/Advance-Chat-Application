ca03003e539d106f6c1a5e529e89af7e214e64c9
Tanvi1829 <mapuskartanvi6@gmail.com>
2025-09-18 16:25:10 +0530

    auth middleware changes

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -7,21 +7,10 @@ export const protectRoute = async (req, res, next) => {
     const token = req.cookies.jwt;
     if (!token) return res.status(401).json({ message: "Unauthorized - No token provided" });
 
-    let decoded;
-    try {
-      decoded = jwt.verify(token, ENV.JWT_SECRET);
-    } catch (err) {
-      if (err.name === "TokenExpiredError") {
-        return res.status(401).json({ message: "Unauthorized - Token expired" });
-      } else {
-        return res.status(401).json({ message: "Unauthorized - Invalid token" });
-      }
-    }
-
+    const decoded = jwt.verify(token, ENV.JWT_SECRET);
     if (!decoded) return res.status(401).json({ message: "Unauthorized - Invalid token" });
 
-    // jwt.sign({ id: userId }) so use decoded.id
-    const user = await User.findById(decoded.id).select("-password");
+    const user = await User.findById(decoded.userId).select("-password");
     if (!user) return res.status(404).json({ message: "User not found" });
 
     req.user = user;
