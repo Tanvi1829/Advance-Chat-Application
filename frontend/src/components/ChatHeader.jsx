e667844b803a4ab8914980276393cc29839338c7
Tanvi1829 <mapuskartanvi6@gmail.com>
2025-10-03 14:09:21 +0530

    Add some new changes

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,7 +1,8 @@
-import { XIcon } from "lucide-react";
+import { ChevronDown, MoreVertical, Phone, PhoneIcon, VideoIcon, XIcon } from "lucide-react";
 import { useChatStore } from "../store/useChatStore";
 import { useEffect } from "react";
 import { useAuthStore } from "../store/useAuthStore";
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 
 function ChatHeader() {
   const { selectedUser, setSelectedUser, typingUsers } = useChatStore();
@@ -47,12 +48,84 @@ function ChatHeader() {
           ) : (
             <p className="text-slate-400 text-sm">{isOnline ? "Online" : "Offline"}</p>
           )}
+
         </div>
       </div>
 
-      <button onClick={() => setSelectedUser(null)}>
+<div className="flex gap-6">
+
+            <PhoneIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
+            <VideoIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
+      {/* <button onClick={() => setSelectedUser(null)}>
         <XIcon className="w-5 h-5 text-slate-400 hover:text-slate-200 transition-colors cursor-pointer" />
-      </button>
+      </button> */}
+     <Menu as="div" className="relative inline-block">
+      <MenuButton >
+        {/* Options */}
+        <MoreVertical aria-hidden="true" className="-mr-1 size-5 text-gray-400" />
+      </MenuButton>
+
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-white/10 rounded-md bg-gray-800 outline-1 -outline-offset-1 outline-white/10 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:text-white data-focus:outline-hidden"
+            >
+              Edit Contact
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:text-white data-focus:outline-hidden"
+            >
+              Archive Chat
+            </a>
+          </MenuItem>
+        </div>
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:text-white data-focus:outline-hidden"
+            >
+              Share Contact
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:text-white data-focus:outline-hidden"
+            >
+              Add to favorites
+            </a>
+          </MenuItem>
+        </div>
+        <div className="py-1">
+          <MenuItem>
+            <a
+              href="#"
+              className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:text-white data-focus:outline-hidden"
+            >
+              Delete Chat
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <button
+             onClick={() => setSelectedUser(null)}
+              className="block px-4 py-2 text-sm text-gray-300 data-focus:bg-white/5 data-focus:text-white data-focus:outline-hidden"
+            >
+              Close Chat
+            </button>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </Menu>
+</div>
     </div>
   );
 }
